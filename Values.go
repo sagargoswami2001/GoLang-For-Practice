a2f771a81c266b7826e61cc975c8c5bee53caa34
Sagar Goswami <88379870+sagargoswami2001@users.noreply.github.com>
2024-04-03 09:32:33 +0530

    File

---
+++ Values.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+func main()	{
+	fmt.Println("Sagar" + "Goswami")
+
+	fmt.Println("18+9 =", 18+9)
+	fmt.Println("9.0/3.0 =", 9.0/3.0)
+
+	fmt.Println(true && false)
+	fmt.Println(true || false)
+	fmt.Println(!true)
+}
